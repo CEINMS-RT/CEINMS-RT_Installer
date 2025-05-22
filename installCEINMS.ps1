0921e6f95b5120c0bf7fcc764ac47faef9e41551
Roger van Kanten <roger.van.kanten@gmail.com>
2025-05-22 15:23:30 +0200

    Download and execute CMake v4.0.2 installer

--- installCEINMS.ps1
+++ installCEINMS.ps1
@@ -8,7 +8,8 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 # Installs CMake
 #===============================================================
 
-Start-Process -Wait .\cmake-3.23.0-rc1-windows-x86_64.msi -ArgumentList "/passive"
+Invoke-WebRequest -Uri https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2-windows-x86_64.msi -OutFile cmake.msi
+Start-Process -Wait .\cmake.msi -ArgumentList "/passive"
 # Current user path variables for CMake
 if ($env:PATH -notcontains "$env:PROGRAMFILES\CMake\bin") { 
     $UserPath  = [System.Environment]::GetEnvironmentVariable("Path","User")
